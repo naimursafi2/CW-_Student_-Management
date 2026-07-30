5307c7449aac33e1f7b9438868716570ac68a6c7
naimursafi2 <naimursafi4@gmail.com>
2026-07-30 14:17:25 +0600

    getPendingUsers

--- backend-auth/controllers/adminController.js
+++ backend-auth/controllers/adminController.js
@@ -1,10 +1,24 @@
 const { ReturnDocument } = require("mongodb");
 const User = require("../models/User");
 
+function formatUser(user) {
+  return {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    profilePicture: user.profilePicture,
+    bio: user.bio,
+    address: user.address,
+    role: user.role,
+    isApproved: user.isApproved,
+    isEmailVerfied: user.isEmailVerfied,
+    createdAt: user.createdAt,
+  };
+}
 
 const getAllUsers = async (req, res, next) => {
   try {
-    const alluser = await User.find({role: ["student", "teacher"]});
+    const alluser = await User.find({ role: ["student", "teacher"] });
     return res
       .status(200)
       .send({ success: true, message: "user get successfully", user: alluser });
@@ -17,27 +31,97 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
-const approvedUserRole = async(req,res)=>{
- try{
-   const params = req.params.id;
-  const updateRole = await User.findByIdAndUpdate(params, {
-    isApproved: true,
+const approvedUserRole = async (req, res) => {
+  try {
+    const params = req.params.id;
+    const updateRole = await User.findByIdAndUpdate(
+      params,
+      {
+        isApproved: true,
+      },
+      { ReturnDocument: "after" },
+    );
+    return res
+      .status(200)
+      .send({
+        success: true,
+        message: "role updated successfully",
+        data: { user: updateRole },
+      });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({
+        success: false,
+        message: "role updated failed",
+        error: error.message,
+      });
+  }
+};
 
-  },{ReturnDocument:"after"})
-return res.status(200).send({success: true, message: "role updated successfully", data: {user: updateRole}})
- }catch(error){
-  return res.status(500).send({success: false, message: "role updated failed", error: error.message})
- }
-}
+const deleteUser = async (req, res) => {
+  try {
+    const params = req.params.id;
+    const deleteUser = await User.findByIdAndDelete(params);
+    return res
+      .status(200)
+      .send({ success: true, message: "user deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({
+        success: false,
+        message: "user delet failed",
+        error: error.message,
+      });
+  }
+};
 
-const deleteUser = async (req, res)=>{
-  try{
-   const params = req.params.id;
-  const deleteUser = await User.findByIdAndDelete(params)
-return res.status(200).send({success: true, message: "user deleted successfully"})
- }catch(error){
-  return res.status(500).send({success: false, message: "user delet failed", error: error.message})
- }
-}
+const getPendingUsers = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
 
-module.exports = {getAllUsers,approvedUserRole, deleteUser};
+    const filter = {
+      isApproved: false,
+      role: { $in: ["teacher", "student"] },
+    };
+
+    const total = await user.countDocuments(filter);
+    const pendingUsers = await user
+      .find(filter)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const totalPages = Math.ceil(total / limit);
+
+    return res.status(200).send({
+      success: true,
+      message: "Pending users retrieved successfully",
+      data: {
+        users: pendingUsers.map(formatUser),
+        pagination: {
+          currentPage: page,
+          totalPages,
+          totalItems: total,
+          itemPerPage: limit,
+          hasnextPage: page < totalPages,
+          hasPrevPage: Page > 1,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({
+        success: false,
+        message: "Internal Server Error",
+        error: error.message,
+      });
+  }
+};
+
+module.exports = { getAllUsers, approvedUserRole, deleteUser, getPendingUsers };
--- backend-auth/routes/api/admin.js
+++ backend-auth/routes/api/admin.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {getAllUsers,approvedUserRole, deleteUser} = require("../../controllers/adminController");
+const {getAllUsers,approvedUserRole, deleteUser, getPendingUsers} = require("../../controllers/adminController");
 const router = express.Router();
 const { protect, requireAdmin } = require("../../middleware/authMiddleware.js");
 
@@ -8,6 +8,7 @@ router.use(protect,requireAdmin)
 router.get("/users", getAllUsers)
 router.patch("/approved/:id" , approvedUserRole)
 router.delete("/delete/:id",deleteUser)
+router.get("/users/pending", getPendingUsers)
 
 
 
